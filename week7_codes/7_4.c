01f6670dbbce9864a5964d1ec9f9504cc738199c
Hitham Jleed <hitham.ly@gmail.com>
2023-11-01 22:59:50 -0400

    Update 7_4.c

--- week7_codes/7_4.c
+++ week7_codes/7_4.c
@@ -8,34 +8,31 @@ Q4. Write a C program to enter 10 elements in array and find frequency of each e
 #include <stdio.h>
 
 int main() {
-    int arr[10];
-    int freq[10] = {}; // Array to store the frequency of each element
-    int i, j, count;
+    int arr[10]={10, 20, 10, 4, 50, 10, 20, 60, 30, 45};
+    int freq[10]={-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};// Initialize frequency array with -1
+    int count;
 
-    // Input 10 elements from the user
-    printf("Enter 10 elements:\n");
-    for(i = 0; i < 10; i++) {
-        printf("Enter element %d: ", i+1);
-        scanf("%d", &arr[i]);
-    }
-
-    // Calculate the frequency of each element
-    for(i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
         count = 1;
-        for(j = i+1; j < 10; j++) {
-            if(arr[i] == arr[j]) {
+        for (int j = i + 1; j < 10; j++) {
+            if (arr[i] == arr[j]) {
                 count++;
+                freq[j] = 0; // Marking the counted element as 0
             }
         }
-        freq[i] = count;
-        
+        if (freq[i] != 0) {
+            freq[i] = count;
+        }
     }
 
-    // Print the frequency of each element
-    printf("\nFrequency of each element:\n");
-    for(i = 0; i < 10; i++) {
-        printf("Frequency of %d: %d\n", arr[i], freq[i]);
+    printf("\nElement | Frequency\n");
+    printf("--------------------\n");
+    for (int i = 0; i < 10; i++) {
+        if (freq[i] != 0) {
+            printf("%6d | %9d\n", arr[i], freq[i]);
+        }
     }
 
     return 0;
 }
+
