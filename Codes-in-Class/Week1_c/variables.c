1237d116c4ec962afe10e86162ec53834cfc6a5d
Hitham Jleed <hitham.ly@gmail.com>
2025-12-02 20:15:13 -0500

    Add files via upload

---
+++ Codes-in-Class/Week1_c/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+    float realA;
+    realA=6.5;
+	num1=5.4;
+	num2=8;
+    sum = num1 + num2;
+    printf("The sum of %d and %d is: %d\n",num1,num2, sum);
+    printf("realA = %f \n",realA);
+
+    return 0;
+}
